86a7d9613e9407b5433aecb355b7e2a556bb828c
ajay2vj <vjajay98@gmail.com>
2022-05-04 08:11:21 +0530

    login text added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Login from './components/login';
 function App() {
   return (
     <Row className='py-40'>
+      <Col span={24} className='text-center pb-6'>
+        <span className='text-2xl text-center font-mono'>
+          NCD LOGIN
+        </span>
+      </Col>
       <Col span={12} offset={6}>
         <Login />
       </Col>
